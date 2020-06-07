eb547247bfae0218bc7ff9f9a1a919f07e55b982
mengxiangtong <928593829@qq.com>
2020-06-07 16:54:27 +0800

    2

--- miaomiao/src/App.vue
+++ miaomiao/src/App.vue
@@ -1,8 +1,7 @@
 <template>
-    <keep-alive>
-		 <!-- 留坑，非常重要 -->
-        <router-view/>
-    </keep-alive>
+	<keep-alive>
+		<!-- 留坑，非常重要 -->
+		<!-- 路由匹配到的组件将显示在这里 -->
+		<router-view />
+	</keep-alive>
 </template>
-
-
