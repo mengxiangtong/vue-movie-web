3074176eede6db8dddedfa2e2da1d1b91872e5c3
mengxiangtong <928593829@qq.com>
2020-06-08 22:20:42 +0800

    优化首页代码

--- miaomiao/src/routers/index.js
+++ miaomiao/src/routers/index.js
@@ -7,6 +7,7 @@ import adminRouter from './admin'
 
 Vue.use(Router)//挂载属性
 //要在嵌套的出口中渲染组件，需要在 VueRouter 的参数中使用 children 配置
+//定义 路径到 组件的 映射。
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
